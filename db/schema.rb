3cbe287dd28e7c35c5c47188eb9db3549970a6d7
Rojesh Tamrakar <rojesh.tmkr@gmail.com>
2020-04-23 20:25:06 +0545

    Add admin requirement to add categories and admin feature in app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_052348) do
+ActiveRecord::Schema.define(version: 2020_04_23_133403) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_04_23_052348) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
